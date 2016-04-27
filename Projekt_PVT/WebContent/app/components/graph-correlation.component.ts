a68325aa3a75c22de80e003434e6c659e00fc9ae
ndi14psd <ndi14psd@student.hig.se>
2016-04-27 10:01:44 +0200

    data dyker upp när man hovrar över punkter

--- Projekt_PVT/WebContent/app/components/graph-correlation.component.ts
+++ Projekt_PVT/WebContent/app/components/graph-correlation.component.ts
@@ -1,10 +1,12 @@
 import {Component, Input, OnChanges, SimpleChange} from 'angular2/core';
-import {CHART_DIRECTIVES} from 'angular2-highcharts';
+import {CHART_DIRECTIVES, Highcharts} from 'angular2-highcharts';
 import {ChooseSource} from './choose-datasource.component';
 import {DatasourceService} from '../service/datasource.service';
 import {IDatasource} from '../interface/datasource';
 import {DataSourceJson} from '../interface/datasource-json';
 import {HTTP_PROVIDERS} from 'angular2/http';
+
+Highcharts
 
 @Component({
     selector: 'graph-correlation',
@@ -14,13 +16,26 @@ import {HTTP_PROVIDERS} from 'angular2/http';
 })
 export class GraphCorrelationComponent implements OnChanges {
 
-    options: Object;
+    chart: HighchartsChartObject;
+    options: HighchartsOptions;
     errorMessage: string;
     datasource: DataSourceJson;
     @Input() sourceOne: string;
     @Input() sourceTwo: string;
 
     constructor(private dataSourceService: DatasourceService) {
+    }
+    
+    saveInstance(chart: HighchartsChartObject) {
+        this.chart = chart;
+    }
+    
+    onPointHover(point: HighchartsPointObject) : void {
+        this.chart.options.tooltip.formatter = () : string => {
+            return "<b>Date: " + point.date + "</b><br>" + 
+            "source1: " + point.x + "<br>" +
+            "source2: " + point.y;
+        }
     }
     
     plot() {
@@ -31,13 +46,20 @@ export class GraphCorrelationComponent implements OnChanges {
                 () => this.plotGraph());
     }
     
+    
+    
     private plotGraph() : void {
         this.options = {
             title: { text: this.sourceOne },
+            plotOptions: {
+              area: { turboThreshold: 0 }  
+            },
+            tooltip: {
+                formatter: (): boolean => false
+            },
             series: [{
                 data: this.datasource.data,
                 type: 'scatter',
-                turboThreshold: 0
             }]
         };
     }
