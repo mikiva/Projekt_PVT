ead6a2bd180c7793ceb30e5ed813990a4f27c3d5
Jonas Öster <jonas_leker@hotmail.com>
2016-04-29 11:30:51 +0200

    undermeny funkar med alla funktioner
    behövs lite refaktorerar

--- Projekt_PVT/WebContent/app/components/choose-datasource.component.ts
+++ Projekt_PVT/WebContent/app/components/choose-datasource.component.ts
@@ -11,10 +11,9 @@ import {Menu} from '../interface/menu';
 export class ChooseSource implements OnInit {
     @Input() input: string = "hej";
     @Output() output: EventEmitter<string> = new EventEmitter<string>();
-    @Output() index: EventEmitter<number>= new EventEmitter<number>();
+    @Output() output1: EventEmitter<string>= new EventEmitter<string>();
     menu: Menu[];
     underMenu: String[];
-    underMenuState: boolean = false;
     errorMessage: string;
     
     constructor(private datasourceService: DatasourceService) {
@@ -30,11 +29,15 @@ export class ChooseSource implements OnInit {
 
     private fillUnderMenu(index): void {
         this.underMenu = this.menu[index].values;
-        this.underMenuState = true;
         console.log(this.menu);
     }
     
-    public onClick(value, index) {
+    public onUnderMenuClick(value): void {
+        this.output1.emit(value);
+        console.log(value);
+    }
+    
+    public onMenuClick(value, index): void {
         this.output.emit(value);
         this.fillUnderMenu(index);
         console.log(value);
