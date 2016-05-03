741ea809ec66ce19ef0e5564622fe2b9f4b1cc55
Jonatan Högberg <ndi14jhg@student.hig.se>
2016-05-03 15:57:31 +0200

    Skapa rubrik knapp
    Lagt till en knapp som är kopplat till en funktion "makeHeader" i en
    typescript component "header.component.ts". Dock skapas inte rubriken än

---
+++ Projekt_PVT/WebContent/app/components/header.component.ts
@@ -0,0 +1,14 @@
+import {Input, Component} from 'angular2/core';
+
+@Component({
+})
+
+export class header {
+        
+    constructor() {
+    }
+    
+    public function makeHeader(){
+        alert('foo');
+    }
+}
