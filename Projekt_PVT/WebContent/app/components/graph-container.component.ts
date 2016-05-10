ecb3231c17b2250a05b2a8c6207238c0613f8f3e
ndi14min <ndi14min@student.hig.se>
2016-05-10 14:19:17 +0200

    Ingen regression visas om endast ett matchat data finns i korrelationsdiagrammet

--- Projekt_PVT/WebContent/app/components/graph-container.component.ts
+++ Projekt_PVT/WebContent/app/components/graph-container.component.ts
@@ -3,14 +3,9 @@ import {Component, Output} from 'angular2/core';
 import {Graph} from './graph.component';
 import {GraphCorrelationComponent} from './graph-correlation.component';
 import {ChooseSource} from './choose-datasource.component';
-
+import {ChooseResolution} from './choose-resolution.component';
 import {SampleDatePicker} from './sampleapp';
 
-@Component({
-    selector: 'graph-container',
-    directives: [Graph, GraphCorrelationComponent, ChooseSource, SampleDatePicker],
-
-import {ChooseResolution} from './choose-resolution.component';
 
 @Component({
     selector: 'graph-container',
@@ -18,23 +13,23 @@ import {ChooseResolution} from './choose-resolution.component';
     templateUrl: 'app/html/graph-container.html',
 })
 
-export class GraphContainerComponent {    
-    
+export class GraphContainerComponent {
+
     sourceOne: Object;
     sourceTwo: Object;
     resolution: string;
-    
-    constructor(){}
-    
-    public setSourceOne(sourceOne: Object) : void {
+
+    constructor() { }
+
+    public setSourceOne(sourceOne: Object): void {
         this.sourceOne = sourceOne;
     }
-    
-    public setSourceTwo(sourceTwo: Object) : void {
+
+    public setSourceTwo(sourceTwo: Object): void {
         this.sourceTwo = sourceTwo;
-    }  
-    
-    public setResolution(resolution : string) : void{
+    }
+
+    public setResolution(resolution: string): void {
         this.resolution = resolution;
-    }   
+    }
 }
