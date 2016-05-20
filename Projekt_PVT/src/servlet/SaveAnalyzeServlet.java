fb516ed232b970adebcaba858a6e09cfe749d032
ndi14psd <ndi14psd@student.hig.se>
2016-05-20 14:56:18 +0200

    Removed unused import

--- Projekt_PVT/src/servlet/SaveAnalyzeServlet.java
+++ Projekt_PVT/src/servlet/SaveAnalyzeServlet.java
@@ -7,8 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.mockito.asm.util.CheckAnnotationAdapter;
-
 import analysis.Analysis;
 import analysis.DatabaseWithSource;
 import analysis.DateRange;
