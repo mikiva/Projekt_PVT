89bb2a6586cc85d4d6f89d6a01136f77d770e87c
ndi14psd <ndi14psd@student.hig.se>
2016-05-24 15:05:19 +0200

    Back to the Statement

--- Projekt_PVT/src/analysis/database/SqlDatabase.java
+++ Projekt_PVT/src/analysis/database/SqlDatabase.java
@@ -75,11 +75,7 @@ public class SqlDatabase {
 		List<Title> titles = new ArrayList<>();
 		
 		try (Connection conn = table.connectToDatabase()) {
-			String query = "SELECT * FROM \"?\")";
-			PreparedStatement ps = conn.prepareStatement(query);
-			ps.setString(1, table.name());			
-			ResultSet rs = ps.executeQuery();
-			//ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM \"" + table.name() + "\"");
+			ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM \"" + table.name() + "\"");
 			while (rs.next()) {
 				titles.add(new Title(rs.getString("TITLE")));
 			}
