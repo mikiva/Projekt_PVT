089cb32bf78ee627e4a2296f0758824f7654cc98
ndi14min <ndi14min@student.hig.se>
2016-05-24 15:37:42 +0200

    missar jesu födelse igen

--- Projekt_PVT/WebContent/src/app/graph-container/shared/database.service.ts
+++ Projekt_PVT/WebContent/src/app/graph-container/shared/database.service.ts
@@ -6,29 +6,29 @@ import {IDatasource} from './datasource';
 
 
 @Injectable()
-export class DatabaseService{
+export class DatabaseService {
 
     private saveUrl = 'http://rigel.hig.se:8080/Bulle-dev/SaveAnalyzeServlet?';
     //private saveUrl = 'http://rigel.hig.se:8080/Bulle-dev/ServletTest?';
 
     private url: string;
-    
-    constructor(private http: Http){}
 
-    public saveAnalysis(dateBefore: string, dateAfter: string, resolution: string, sourceOne: Object, sourceTwo: Object, title: string): Observable<string>{
+    constructor(private http: Http) { }
+
+    public saveAnalysis(dateBefore: string, dateAfter: string, resolution: string, sourceOne: Object, sourceTwo: Object, title: string): Observable<string> {
 
         this.url = this.getSaveUrl(sourceOne, sourceTwo, resolution, dateBefore, dateAfter, title);
         console.log(this.url);
-     
+
         this.http.get(this.url)
             .map((response: Response) => <string> response.toString())
             .do(data => console.log('Something works'))
-            .catch(new Error()); 
+            .catch(new Error());
     }
-    public getSaveUrl(sourceOne: Object, sourceTwo: Object, resolution: string, dateBefore: string, dateAfter: string, title: string){
-        return (this.saveUrl + 'title='+ (title? title: 'NoTitle') + '&res=' + (resolution? resolution : 'day') + '&database1=' + sourceOne["database"] + '&value1=' + sourceOne["dataset"] +
-            (sourceTwo? '&database2=' + sourceTwo["database"] + '&value2=' + sourceTwo["dataset"] : "")
-            + '&startDate=' + (dateBefore || '0001-01-01') + '&endDate=' + (dateAfter || '9999-12-30'));
-        
+    public getSaveUrl(sourceOne: Object, sourceTwo: Object, resolution: string, dateBefore: string, dateAfter: string, title: string) {
+        return (this.saveUrl + 'title=' + (title ? title : 'NoTitle') + '&res=' + (resolution ? resolution : 'day') + '&database1=' + sourceOne["database"] + '&value1=' + sourceOne["dataset"] +
+            (sourceTwo ? '&database2=' + sourceTwo["database"] + '&value2=' + sourceTwo["dataset"] : "")
+            + '&startDate=' + (dateBefore || '0001-01-01') + '&endDate=' + (dateAfter || '9998-12-30'));
+
     }
 }
