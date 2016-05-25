6ca42c46974cdecd5a3d2d0fba249a0d1484c712
ndi14min <ndi14min@student.hig.se>
2016-05-25 15:34:42 +0200

    la till parenteser på Injectable

--- Projekt_PVT/WebContent/src/app/graph-container/shared/loadData.service.ts
+++ Projekt_PVT/WebContent/src/app/graph-container/shared/loadData.service.ts
@@ -4,7 +4,7 @@ import {Http, Response} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import {IDatasource} from './datasource';
 
-@Injectable
+@Injectable()
 export class LoadDataService{
     
     constructor(private http: Http){}
