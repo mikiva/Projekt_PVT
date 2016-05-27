0e1ad43be04c01fc58c96379109efad383deb3c1
Jonas Öster <jonas_leker@hotmail.com>
2016-05-27 10:59:49 +0200

    ändrat så dropdown inte blir för stor

--- Projekt_PVT/WebContent/src/app/graph-container/shared/loadData.service.ts
+++ Projekt_PVT/WebContent/src/app/graph-container/shared/loadData.service.ts
@@ -5,45 +5,30 @@ import {Observable} from 'rxjs/Observable';
 import {IDatasource} from './datasource';
 
 @Injectable()
-export class LoadDataService{
-    
-    constructor(private http: Http){}
-    
+export class LoadDataService { 
     private title : string;
-    
     private availableUrl = "http://rigel.hig.se:8080/Bulle-dev/GetAvailableAnalysisServlet"
-    
     private savedUrl = "http://rigel.hig.se:8080/Bulle-dev/GetAnalysisServlet?title="
     
-    
-    
+    constructor(private http: Http){}
+       
     public getSaved(){
-        
         return this.http.get(this.availableUrl)
-        .map((response: Response) => <string[]> response.json().values)
-        .catch(this.handleError);
+            .map((response: Response) => <string[]> response.json().values)
+            .catch(this.handleError);
     }
     
    public loadAnalysis(title:string): Observable<any>{
-    
-   return this.http.get(this.getLoadUrl(title))
+        return this.http.get(this.getLoadUrl(title))
             .map((response: Response) => <any> response.json())
-            .catch(this.handleError);
-      
+            .catch(this.handleError);  
     }
-       private handleError(error: Response) {
+    
+    private handleError(error: Response) {
         return Observable.throw(error.json().error || 'Server error');
     }
     
-      
-    
-  private  getLoadUrl(title: string){
-        
-        return (this.savedUrl + title);
-        
-        
+    private  getLoadUrl(title: string){
+        return (this.savedUrl + title);    
     }
-    
-    
-    
 }
