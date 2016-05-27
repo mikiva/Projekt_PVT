d4f3cb747841317133eda4a33c6904824cb260a6
ndi14min <ndi14min@student.hig.se>
2016-05-27 10:51:48 +0200

    ändrat url för att spara kommentar

--- Projekt_PVT/WebContent/src/app/graph-container/shared/database.service.ts
+++ Projekt_PVT/WebContent/src/app/graph-container/shared/database.service.ts
@@ -20,7 +20,7 @@ export class DatabaseService {
     public saveAnalysis(dateBefore: string, dateAfter: string, resolution: string, sourceOne: Object, sourceTwo: Object, title: string, comment: string): Observable<string> {
 
         this.url = this.getSaveUrl(sourceOne, sourceTwo, resolution, dateBefore, dateAfter, title, comment);
-        console.log(this.url);
+        console.log(comment);
 
         return this.http.get(this.url)
             .map(response => response.text());
@@ -29,7 +29,7 @@ export class DatabaseService {
     public getSaveUrl(sourceOne: Object, sourceTwo: Object, resolution: string, dateBefore: string, dateAfter: string, title: string, comment:string) {
         return (this.saveUrl + 'title=' + (title ? title : 'NoTitle') + '&res=' + (resolution ? resolution : 'day') + '&database1=' + sourceOne["database"] + '&value1=' + sourceOne["dataset"] +
             (sourceTwo ? '&database2=' + sourceTwo["database"] + '&value2=' + sourceTwo["dataset"] : "")
-            + '&startDate=' + (dateBefore || '0001-01-01') + '&endDate=' + (dateAfter || '9998-12-30'));
+            + '&startDate=' + (dateBefore || '0001-01-01') + '&endDate=' + (dateAfter || '9998-12-30') + '&comment='+comment);
 
     }
 }
