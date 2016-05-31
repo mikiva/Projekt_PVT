71b081be89eea8a1ce963150beab796346bd8b55
Jonas Öster <jonas_leker@hotmail.com>
2016-05-31 14:18:09 +0200

    skapat delete servlet

---
+++ Projekt_PVT/src/servlet/DeleteAnalysisServlet.java
@@ -0,0 +1,43 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import analysis.database.AnalysisTable;
+import analysis.database.SqlDatabase;
+
+/**
+ * Servlet implementation class DeleteAnalysisServlet
+ */
+@WebServlet("/DeleteAnalysisServlet")
+public class DeleteAnalysisServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteAnalysisServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json;charset=UTF-8");
+		
+		SqlDatabase db = new SqlDatabase(AnalysisTable.getInstance());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
