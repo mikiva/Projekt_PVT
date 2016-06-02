eac6b3d95490adf5a84657dcf20aa9ebb6867d50
ndi14min <ndi14min@student.hig.se>
2016-06-02 14:12:13 +0200

    update

--- Projekt_PVT/WebContent/src/app/graph-container/choose-datasource/choose-datasource.component.ts
+++ Projekt_PVT/WebContent/src/app/graph-container/choose-datasource/choose-datasource.component.ts
@@ -14,64 +14,63 @@ export class ChooseSource implements OnInit {
     @Input() sourceInput: Object;
     source: Object = null;
     database: string;
-    
+
     @Input() sM: string;
     @Input() sUm: string;
 
     menu: Menu[];
     underMenu: String[][];
     errorMessage: string;
-    
+
     constructor(private datasourceService: DatasourceService) {
     }
-    
+
     ngOnInit(): void {
         this.datasourceService.getMenu()
             .subscribe(
                 menu => this.menu = menu,
-                error =>  this.errorMessage = <any>error);
+                error => this.errorMessage = <any>error);
 
     }
-    
+
     onUnderMenuClick(index: number): void {
-        this.source = {database: this.database, dataset: this.underMenu[index][0]};
+        this.source = { database: this.database, dataset: this.underMenu[index][0] };
         this.sourceOutput.emit(this.source);
     }
-    
+
     onMenuClick(value, index: number): void {
         this.database = this.menu[index].database_link;
         this.fillUnderMenu(index);
     }
 <<<<<<< HEAD
-
     private fillUnderMenu(index: number): void {
         this.underMenu = this.menu[index].values;
     }
-    update(){
-        
-      var m : HTMLSelectElement = (<HTMLSelectElement> document.getElementById("menu"));
-      var uM = (<HTMLSelectElement>document.getElementById("underMenu"));
-      
-      for(var i = 0; i < m.options.length; i++){
-          if(m.options[i].text == this.sM)
-          m.options[i].selected = true;
-      }
-      
-      for (var i = 0; i< uM.options.length; i++){
-          if(uM.options[i].text == this.sUm)
-          uM.options[i].selected = true;
-      }
-      
-      
-        
+    update() {
+
+        var m: HTMLSelectElement = (<HTMLSelectElement> document.getElementById("menu"));
+        var uM = (<HTMLSelectElement>document.getElementById("underMenu"));
+
+        for (var i = 0; i < m.options.length; i++) {
+            if (m.options[i].text == this.sM)
+                m.options[i].selected = true;
+        }
+
+        for (var i = 0; i < uM.options.length; i++) {
+            if (uM.options[i].text == this.sUm)
+                uM.options[i].selected = true;
+        }
+
+
+
     }
 
-=======
-    
-    update(source1:Object, source2:Object){
-        
+
+
+    update(source1: Object, source2: Object) {
+
     }
-    
+
     private fillUnderMenu(index: number): void {
         this.underMenu = this.menu[index].values;
     }
