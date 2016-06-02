2e5a9e0f3f0a9d783811186b3ae4c894d9e8df9d
Jonas Öster <jonas_leker@hotmail.com>
2016-06-02 15:20:41 +0200

    försökt med mockito test för response, request, men funkar ej

--- Projekt_PVT/test/servlet/GetGraphDataServletTest.java
+++ Projekt_PVT/test/servlet/GetGraphDataServletTest.java
@@ -10,47 +10,50 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class GetGraphDataServletTest extends Mockito {
 
 	private GetGraphDataServlet servlet;
-	private HttpServletRequest emptyRequest;
-	private HttpServletResponse emptyResponse;
+	private HttpServletRequest request;
+	private HttpServletResponse response;
 
 	@Before
 	public void setUp() throws Exception {
 		servlet = new GetGraphDataServlet();
-		emptyRequest = mock(HttpServletRequest.class);       
-		emptyResponse = mock(HttpServletResponse.class);
+		request = mock(HttpServletRequest.class);       
+		response = mock(HttpServletResponse.class);
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		servlet = null;
 	}
-
+	
+	@Ignore
 	@Test
-	public void testEmptyParameters() {
-
-		when(emptyRequest.getParameter("title")).thenReturn("");
-		when(emptyRequest.getParameter("res")).thenReturn("");
-		when(emptyRequest.getParameter("startDate")).thenReturn("");
-		when(emptyRequest.getParameter("endDate")).thenReturn("");
-		when(emptyRequest.getParameter("database1")).thenReturn("");
-		when(emptyRequest.getParameter("value1")).thenReturn("");
-		when(emptyRequest.getParameter("database2")).thenReturn("");
-		when(emptyRequest.getParameter("value2")).thenReturn("");
+	public void testCorrectParameters() {
+		when(request.getParameter("res")).thenReturn("Month");
+		when(request.getParameter("database1")).thenReturn("ENTSOE");
+		when(request.getParameter("value1")).thenReturn("DE_CONS");
+		when(request.getParameter("database2")).thenReturn("ENTSOE");
+		when(request.getParameter("value2")).thenReturn("FI_PROD");
+		when(request.getParameter("startDate")).thenReturn("2003-06-06");
+		when(request.getParameter("endDate")).thenReturn("2016-06-02");
 
 		try {
-			servlet.doGet(emptyRequest, emptyResponse);
+			servlet.doGet(request, response);
 		} catch (ServletException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
+		System.out.println(response.getContentType());
+		
+		assertEquals(response.getContentType(), "application/json;charset=UTF-8");
+		//assertThat(emptyResponse.toString(), containsString("could not be parsed"));
 	}
 
 }
