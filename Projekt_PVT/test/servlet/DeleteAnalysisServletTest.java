01c50855649422649ca5677c67b2f6e8a53b06cb
ndi14psd <ndi14psd@student.hig.se>
2016-06-03 12:50:21 +0200

    gör tester på DeleteAnalysisServlet

--- Projekt_PVT/test/servlet/DeleteAnalysisServletTest.java
+++ Projekt_PVT/test/servlet/DeleteAnalysisServletTest.java
@@ -3,36 +3,60 @@ package servlet;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.io.Writer;
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import analysis.Title;
 import analysis.database.SqlDatabase;
 
 public class DeleteAnalysisServletTest {
 
-	@Test
-	public void deleteAnalysisSuccessfully() throws IOException {
+	private DeleteAnalysisServlet servlet;
+	private Set<Title> savedTitles;
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+	private StringWriter stringWriter;
+	
+	private static final String TITLE_NAME = "test";
+
+	@Before
+	public void setUp() throws Exception {
 		SqlDatabase mockDB = mock(SqlDatabase.class);
-		Title title = new Title("test");
-		Set<Title> savedTitles = new HashSet<>();
-		savedTitles.add(new Title("test"));
+		servlet = new DeleteAnalysisServlet(mockDB);
+		savedTitles = new HashSet<>();
+
 		when(mockDB.getSavedTitles()).thenReturn(savedTitles);
-		PrintWriter writer = new PrintWriter(new StringWriter());
-		HttpServletRequest req = mock(HttpServletRequest.class);
-		HttpServletResponse resp = mock(HttpServletResponse.class);
-		when(req.getParameter("title")).thenReturn("test");
-		when(resp.getWriter()).thenReturn(writer);
-		
+		request = mock(HttpServletRequest.class);
+		response = mock(HttpServletResponse.class);
+		when(request.getParameter("title")).thenReturn(TITLE_NAME);
+
+		stringWriter = new StringWriter();
+		PrintWriter writer = new PrintWriter(stringWriter);
+		when(response.getWriter()).thenReturn(writer);
+
+	}
+
+	@Test
+	public void deleteAnalysisSuccessfully() throws Exception {
+		savedTitles.add(new Title(TITLE_NAME));
+
+		servlet.doGet(request, response);
+
+		assertEquals("Analysis " + TITLE_NAME + " deleted", stringWriter.toString());
+	}
+
+	@Test
+	public void tryToDeleteAnalysisThatDoesNotExist() throws Exception {
+
 	}
 
 }
