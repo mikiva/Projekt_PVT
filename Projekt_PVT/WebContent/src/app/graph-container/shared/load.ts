e33d3a30289aa1feb90ec6eebded8ac60a8cae62
ndi14min <ndi14min@student.hig.se>
2016-06-03 12:14:27 +0200

    interface load data

---
+++ Projekt_PVT/WebContent/src/app/graph-container/shared/load.ts
@@ -0,0 +1,14 @@
+export interface LoadDataID{
+    
+    title: string;
+    database1: string;
+    datasource1: string;
+    database2: string;
+    datasource2: string;
+    resolution: string;
+    startDate: string;
+    endDate: string;
+    comment: string;
+    
+    
+}
